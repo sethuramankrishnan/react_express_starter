e7ac1a91f24e7fbaf7052433cf6814e07065b0ee
sethuramankrishnan <48673734+sethuramankrishnan@users.noreply.github.com>
2020-02-11 21:30:29 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to sethu</h1>
+          <h1 className="App-title">Welcome to React DOCKER</h1>
         </header>
         <Customers />
       </div>
